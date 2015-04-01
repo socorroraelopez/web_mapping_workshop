858e8abaa66c51bbbbca7e385c8182b23a65f298
socorroraelopez <socorroraelopez@gmail.com>
2015-03-31 20:30:54 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -11,3 +11,16 @@ var map = L.mapbox.map('map', mapId);
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
 
+var dataFiletoAdd = 'data/parks.geojson'
+var featureLayer = L.mapbox.featureLayer().loadURL(dataFileTo Add).addTo(map);
+featureLayer.om('ready', function(){
+  this.setStyle({
+    'color':'#green',
+    'fillColor': '#green',
+    'weight': 4,
+    'opacity':.6
+  });
+  map.fitBounds(featureLayer.getBounds())
+
+});
+
