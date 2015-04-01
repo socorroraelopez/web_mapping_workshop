cf857fd0396a3c246b22ce96235b07a3bb1f643b
socorroraelopez <socorroraelopez@gmail.com>
2015-03-31 20:57:01 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -25,3 +25,9 @@ featureLayer.on('ready', function(){
 
 });
 
+featureLayer.on('ready',function(){
+  this.eachLayer(function(layer){
+    layer.bindPopup('Hi, Im the park called ' + layer.feature.properties.name)
+  });
+});
+
